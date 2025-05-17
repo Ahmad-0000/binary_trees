ce43d9b4028c0962af8e0927e194874bbed653d8
Ahmad <ahmad.new.m.v@gmial.com>
2025-05-17 23:38:41 +0200

    Pass all required arguments

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,6 +1,4 @@
-#include "stdio.h"
 #include "binary_trees.h"
-
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
@@ -9,6 +7,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
     if (tree == NULL)
         return;
     func(tree->n);
-    binary_tree_preorder(tree->left);
-    binary_tree_preorder(tree->right);
+    binary_tree_preorder(tree->left, func);
+    binary_tree_preorder(tree->right, func);
 }
