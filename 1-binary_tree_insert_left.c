73bd616b6337d1403f7a7f16653ced20f9390abd
Ahmad <ahmad.new.m.v@gmial.com>
2025-05-17 23:01:03 +0200

    Handle and edge case

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,7 +20,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
       if (node == NULL)
           return NULL;
       if (parent->left)
+      {
+          node->left = parent->left;
           parent->left->parent = node;
+      }
       parent->left = node;
       return node;
 }
