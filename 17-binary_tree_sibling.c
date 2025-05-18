e1367bbc0e972bcb823cc34eb01f859254bc4c0d
Ahmad <ahmad.new.m.v@gmial.com>
2025-05-18 15:59:24 +0200

    Find node sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *tree)
+{
+    if (tree == NULL || tree->parent)
+        return NULL;
+    if (tree->parent-left == tree)
+        return tree->parent->right;
+    return tree->parent->left;
+}
