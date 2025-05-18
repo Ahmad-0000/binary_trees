e750be1b4eeb5a795935387e9438ac2df48471ee
Ahmad <ahmad.new.m.v@gmial.com>
2025-05-18 16:19:04 +0200

    Fix a typo

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-binary_tree_t *binary_tree_unclde(binary_tree_t *node)
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
     if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
         return NULL;
