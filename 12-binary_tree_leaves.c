edba75fae8248149b94ab5c7e0b59aca9de29beb
Ahmad <ahmad.new.m.v@gmial.com>
2025-05-18 12:12:49 +0200

    Reduce stack size

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,7 +6,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
     if (tree == NULL)
         return 0;
-    if (binary_tree_is_leaf(tree))
+    if (tree->left == NULL && tree->right == NULL)
         return 1;
     leaves += binary_tree_leaves(tree->left);
     leaves += binary_tree_leaves(tree->right);
