2aab0e1ad6ee27ba0d57147255a0bbc9ac569f3a
Ahmad <ahmad.new.m.v@gmial.com>
2025-05-18 12:09:58 +0200

    Count leaves of a binary tree

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    size_t leaves = 0;
+
+    if (tree == NULL)
+        return 0;
+    if (binary_tree_is_leaf(tree))
+        return 1;
+    leaves += binary_tree_leaves(tree->left);
+    leaves += binary_tree_leaves(tree->right);
+    return leaves;
+}
