9e01bc59a7f04a05f3c3f0b5f5ada47b458f3bd1
Ahmad <ahmad.new.m.v@gmial.com>
2025-05-18 16:12:52 +0200

    Remove a bug

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,9 +2,9 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *tree)
 {
-    if (tree == NULL || tree->parent)
+    if (tree == NULL || tree->parent == NULL)
         return NULL;
-    if (tree->parent-left == tree)
+    if (tree->parent->left == tree)
         return tree->parent->right;
     return tree->parent->left;
 }
