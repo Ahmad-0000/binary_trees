df7e3bb1f8ceb04ec24ce98fa85e23acbce4bdee
Ahmad <ahmad.new.m.v@gmial.com>
2025-05-24 19:23:35 +0200

    Remove unused set variable

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -3,12 +3,11 @@
 bst_t *array_to_bst(int *array, size_t size)
 {
     bst_t *root = NULL;
-    bst_t *new_node;
     size_t i;
 
     if (array == NULL || size == 0)
       return NULL;
     for (i = 0; i < size; i++)
-        new_node = bst_insert(&root, array[i]);
+        bst_insert(&root, array[i]);
     return root;
 }
